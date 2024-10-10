8cfaa3fb149aba1c607961d4dd80bd1ab56c3726
Abhilash Sanne <sanne.abhi@gmail.com>
2024-10-10 15:29:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='qr_code',
-    version='1.0',
+    name='qr_code_lib',
+    version='1.0.0',    
+    description="Python project to encode/decode QR Code",
+    author="Abhilash Sanne",
+    author_email="sanne.abhi@gmail.com",
+    url="https://github.com/abhilashsanne/qr_code_lib",
     packages=find_packages(),
-    install_requires=[
-        'qrcode[pil]',
-        'Pillow',
-        'opencv-python',
-    ],
-    entry_points={
-        'console_scripts': [
-            'qr-encode=qr_code_lib.encoder:generate_qr_code',
-            'qr-decode=qr_code_lib.decoder:decode_qr_code',
-        ],
-    },
-    author='Abhilash Sanne',
-    author_email='sanne.abhi@gmail.com',
-    description='A library for encoding and decoding QR codes',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/sanneabhilash/qr_code',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
